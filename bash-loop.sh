6f1150271df94aa17dbd837923693e6fd3a2860f
ZafarIqbal777 <zafariqbal.itnets@gmail.com>
2024-02-28 20:45:30 +0500

    update bash loop and string

---
+++ bash-loop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash  
+#This is the basic example to print a series of numbers from 1 to 10.  
+  
+for num in {1..10}  
+do  
+echo $num  
+done  
+  
+echo "Series of numbers from 1 to 10."  
